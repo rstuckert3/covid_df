3bec1eabadccb7f981bff422fac942f4284a4c17
rstuckert3 <rodrigostuckert@gmail.com>
2020-06-29 12:07:13 -0300

    Facilita a extração e limpeza dos dados

--- covid.R
+++ covid.R
@@ -4,23 +4,73 @@
 
 # Esse código foi feito para analisar os casos da covid no Distrito Federal.
 # Link para a página de extração: https://covid19.ssp.df.gov.br/extensions/covid19/covid19.html#/
+# Dados do dia 2020-06-23 12h
 
-
+# To Do:
+# - CRIAR variáveis "Tem comorbidade?" e "Status do paciente"
 
 # Pacotes:
 library(dplyr)
-library(ggplot2)
-library(readr) # Para garantir o encoding
-
-
-# Internet:
-#library(rvest)
-#x <- "https://covid19.ssp.df.gov.br/resources/dados/dados-abertos.csv?param=[random]"
-#pagina <- read_html(x)
+library(lubridate) # Manipular datas
 
 
 # Puxa o arquivo
 arquivo <- "dados-abertos.csv"
-df <- read_delim(file = arquivo, delim = ";")
-#df2 <- read.csv2(file = arquivo, as.is = !stringsAsFactors, na.strings = "NA") # Padrão brasileiro, mas sem encoding correto.
+link <- "https://covid19.ssp.df.gov.br/resources/dados/dados-abertos.csv"
+
+df <- data.table::fread(link, encoding = "UTF-8", 
+                        col.names = c("id", "Data", "DataCadastro", "Sexo",
+                                      "FaixaEtaria", "RA", "UF", "EstadoSaude",
+                                      "Pneumopatia", "Nefropatia", "DHematologica",
+                                      "DistMetabolico", "Imunopressao", "Obesidade",
+                                      "Outros", "Cardiovasculopatia")
+)
+
+
+# Bota os valores das comorbidades como binários (Apresenta = 1, não apresenta / NA = 0)
+# EXPLICAÇÃO: de início, pessoas com alguma comorbidade recebiam "Sim"
+# para aquelas que apresentassem, e "Não" para todas as demais, enquanto
+# que quem não apresentasse nenhuma aparecia como NA para todas.
+# Após determinada data, o GDF passou a apenas a categorizar aqueles que
+# apresentassem alguma coisa.
+df <- df %>% 
+  mutate(Pneumopatia = ifelse(Pneumopatia == "Sim", 1, 0), 
+         Nefropatia = ifelse(Nefropatia == "Sim", 1, 0),
+         DHematologica = ifelse(DHematologica == "Sim", 1, 0),
+         DistMetabolico = ifelse(DistMetabolico == "Sim", 1, 0),
+         Imunopressao = ifelse(Imunopressao == "Sim", 1, 0),
+         Obesidade = ifelse(Obesidade == "Sim", 1, 0),
+         Outros = ifelse(Outros == "Sim", 1, 0),
+         Cardiovasculopatia = ifelse(Cardiovasculopatia == "Sim", 1, 0)
+  )
+
+
+# Verifica a importação do arquivo
+str(df) # Estrutura
+head(df) # Início
+tail(df) # Fim
+class(df$DataCadastro) # Classe da coluna data
+
+
+# Corrige a coluna das datas de entrada, e a "Outras comorbidades".
+df <- df %>%
+  mutate(DataCadastro = as.Date(DataCadastro, format = '%d/%m/%Y'))
+
+# Verificando
+class(df$DataCadastro)
+head(df$DataCadastro)
+
+# Backup
+df2 <- df
+
+
+# Número de casos de obesidade no dataframe
+sum(df$Obesidade)
+
+
+df <- df %>% mutate(Comorbidade = sum(Pneumopatia, Nefropatia, `Doença Hematológica`,
+                                      `Distúrbios Metabólicos`, `Imunopressão`, Outros,
+                                      Cardiovasculopatia))
+
+
 
